2afb2233df2919c20be701369487d17c30b3c3a3
takashabe <takashabe.public@gmail.com>
2018-05-05 13:46:25 +0900

    Use interfaces componentes

--- cmd/btcli/btcli.go
+++ cmd/btcli/btcli.go
@@ -2,27 +2,16 @@ package main
 
 import (
 	"fmt"
-	"os"
 
 	prompt "github.com/c-bata/go-prompt"
+	"github.com/takashabe/btcli/api/interfaces"
 )
-
-func dummyExecutor(s string) {
-	fmt.Fprintf(os.Stdout, "Write: %s", s)
-}
-
-func dummyCompleter(d prompt.Document) []prompt.Suggest {
-	s := []prompt.Suggest{
-		{Text: "foo", Description: "bar"},
-	}
-	return s
-}
 
 func main() {
 	fmt.Println("Please select table.")
 	p := prompt.New(
-		dummyExecutor,
-		dummyCompleter,
+		interfaces.Executor,
+		interfaces.Completer,
 	)
 	p.Run()
 }
