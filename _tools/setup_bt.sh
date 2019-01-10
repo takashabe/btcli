0498a17f484e1e6f8befa76848825aae5d4dd89a
takashabe <abe_takashi@cyberagent.co.jp>
2019-01-10 23:47:24 +0900

    tools: Allow empty args

--- _tools/setup_bt.sh
+++ _tools/setup_bt.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
-cbt -project $1 -instance $2 -creds $3 deletetable users
-cbt -project $1 -instance $2 -creds $3 deletetable articles
-cbt -project $1 -instance $2 -creds $3 createtable users
-cbt -project $1 -instance $2 -creds $3 createtable articles
-cbt -project $1 -instance $2 -creds $3 createfamily users d
-cbt -project $1 -instance $2 -creds $3 createfamily articles d
-cbt -project $1 -instance $2 -creds $3 set users 1 d:row=madoka
-cbt -project $1 -instance $2 -creds $3 set users 2 d:row=homura
-cbt -project $1 -instance $2 -creds $3 set users 3 d:row=sayaka
-cbt -project $1 -instance $2 -creds $3 set users 4 d:row=kyouko
-cbt -project $1 -instance $2 -creds $3 set users 4 d:row=anko
-cbt -project $1 -instance $2 -creds $3 set articles 1##1 d:title=madoka_title
-cbt -project $1 -instance $2 -creds $3 set articles 1##1 d:content=madoka_content
-cbt -project $1 -instance $2 -creds $3 set articles 2##1 d:title=homura_title
-cbt -project $1 -instance $2 -creds $3 set articles 2##1 d:content=homura_content
-cbt -project $1 -instance $2 -creds $3 set articles 2##2 d:title=homuhomu_title
-cbt -project $1 -instance $2 -creds $3 set articles 2##2 d:content=homuhomu_content
+project=${1:-test-project}
+instance=${2:-test-instance}
+creds=${3:-dummy}
+
+cbt -project $project -instance $instance -creds $creds deletetable users
+cbt -project $project -instance $instance -creds $creds deletetable articles
+cbt -project $project -instance $instance -creds $creds createtable users
+cbt -project $project -instance $instance -creds $creds createtable articles
+cbt -project $project -instance $instance -creds $creds createfamily users d
+cbt -project $project -instance $instance -creds $creds createfamily articles d
+cbt -project $project -instance $instance -creds $creds set users 1 d:row=madoka
+cbt -project $project -instance $instance -creds $creds set users 2 d:row=homura
+cbt -project $project -instance $instance -creds $creds set users 3 d:row=sayaka
+cbt -project $project -instance $instance -creds $creds set users 4 d:row=kyouko
+cbt -project $project -instance $instance -creds $creds set users 4 d:row=anko
+cbt -project $project -instance $instance -creds $creds set articles 1##1 d:title=madoka_title
+cbt -project $project -instance $instance -creds $creds set articles 1##1 d:content=madoka_content
+cbt -project $project -instance $instance -creds $creds set articles 2##1 d:title=homura_title
+cbt -project $project -instance $instance -creds $creds set articles 2##1 d:content=homura_content
+cbt -project $project -instance $instance -creds $creds set articles 2##2 d:title=homuhomu_title
+cbt -project $project -instance $instance -creds $creds set articles 2##2 d:content=homuhomu_content
